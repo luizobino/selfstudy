e00729404e5ad89a029bd04259aeedc312dd2cd2
luizobino <luiz_obino@hotmail.com>
2022-02-15 23:15:15 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function helloworld(){
+    console.log("Hello World")
+}
+
+const saudacao = () => {
+    var data = new Date()
+    return data.getHours() <= 12? "Bom dia": data.getHours() <= 18? "Boa Tarde": "Boa Noite";
+} 
+
+console.log('A saudação do momento é '+ saudacao());
